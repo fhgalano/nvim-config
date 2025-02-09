4e9f3a9561c666f4f07b192d60166b33fa9237f7
fidel <fhgalano@gmail.com>
2025-02-09 16:29:53 -0600

    added snippets to configuration

--- lua/plugins/autocmp/nvim_cmp.lua
+++ lua/plugins/autocmp/nvim_cmp.lua
@@ -2,18 +2,22 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
+        "saadparwaiz1/cmp_luasnip",
     },
     config = function()
         local cmp = require("cmp")
+
+        require("luasnip.loaders.from_vscode").lazy_load()
+
         cmp.setup({
             snippet = {
                 -- REQUIRED - you must specify a snippet engine
                 expand = function(args)
-                    vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+                    require('luasnip').lsp_expand(args.body)
                 end,
             },
             window = {
-                completion = cmp.config.window.bordered(),
+                completion = cmp.config.window.bordered({scrolloff = 100, col_offset = -100}),
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
@@ -27,6 +31,7 @@ return {
             }),
             sources = cmp.config.sources({
               { name = 'nvim_lsp' },
+              { name = 'luasnip' },
             }, {
               { name = 'buffer' },
             })
